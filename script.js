843c7824c3bda0536e7182a0ea4b2b4da64360ff
Preetisharma9958 <149951367+Preetisharma9958@users.noreply.github.com>
2023-11-26 11:19:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,102 @@
-const questions = [
+const quizData = [
     {
-      question: "What is the capital of France?",
-      options: ["Berlin", "Madrid", "Paris", "Rome"],
-      correctAnswer: "Paris"
+      question: "Who is first wildcard winner of BIGBOSS?",
+      options: ["Elvish yadav", "Nora fatehi", "Aashika", "Orry"],
+      correctAnswer: "Elvish yadav"
     },
     {
-      question: "Which planet is known as the Red Planet?",
-      options: ["Mars", "Venus", "Jupiter", "Saturn"],
-      correctAnswer: "Mars"
+      question: "Which duo is most loved in BIGBOSS OTT2?",
+      options: ["Abhisha", "Elvisha", "Abhiya", "Whoever is your favourite"],
+      correctAnswer: "Abhisha"
     },
     {
-      question: "What is the largest mammal in the world?",
-      options: ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
-      correctAnswer: "Blue Whale"
+      question: "Which two contestent make it to the BIGGBOSS OTT2 FINALE ?",
+      options: ["Pooja Bhatt", "Manisha and Abhishek", "Abhishek and Jiya", "Elvish and Abhishek"],
+      correctAnswer: "Elvish and Abhishek"
     },
     {
-      question: "How many continents are there?",
-      options: ["5", "6", "7", "8"],
-      correctAnswer: "7"
+      question: "Who won BIGGBOSS SEASON 13?",
+      options: ["Abhishek(FUKRA_INSAAN)", "Shidhhart shukla", "Manisha rani", "Elvish yadav"],
+      correctAnswer: "Shidhhart shukla"
     },
     {
-      question: "Who wrote 'Romeo and Juliet'?",
-      options: ["Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"],
-      correctAnswer: "William Shakespeare"
+        question: "What Flower symbolises Friendship?",
+        options: ["Lily","Yellow Rose","Lotus","Whatever your favourite"],
+        correctAnswer:"Yellow Rose"
     }
-  ];
+
+];
   
-  const quizContainer = document.getElementById("quiz");
+  let currentQuestion = 0;
+  let score = 0;
   
-  function displayQuiz() {
-    questions.forEach((question, index) => {
-      const questionElement = document.createElement("div");
-      questionElement.className = "question";
-      questionElement.innerHTML = `
-        <p>${index + 1}. ${question.question}</p>
-        ${question.options.map((option, i) => <input type="radio" name="q${index}" value="${option}" id="q${index}o${i}"><label for="q${index}o${i}">${option}</label><br>).join('')}
+  const quizElement = document.getElementById('quiz');
+  const optionsElement = document.getElementById('options');
+  const resultElement = document.getElementById('result');
+  const scoreElement = document.getElementById('score');
+  const nextButton = document.getElementById('next-btn');
+  
+  function loadQuestion() {
+    const questionData = quizData[currentQuestion];
+    quizElement.innerHTML = questionData.question;
+  
+    const optionsHTML = questionData.options.map((option, index) => `
+      <li>
+        <input type="radio" name="option" value="${option}" id="option${index}">
+        <label for="option${index}">${option}</label>
+      </li>
+    `).join('');
+  
+    optionsElement.innerHTML = optionsHTML;
+  }
+  
+  function checkAnswer() {
+    const selectedOption = document.querySelector('input[name="option"]:checked');
+    if (!selectedOption) return false;
+  
+    return selectedOption.value === quizData[currentQuestion].correctAnswer;
+  }
+  
+  function showResult() {
+    const isCorrect = checkAnswer();
+    if (isCorrect) {
+      score++;
+      resultElement.innerHTML = "Correct!";
+    } else {
+      resultElement.innerHTML = `Wrong! The correct answer was ${quizData[currentQuestion].correctAnswer}.                                                                       Better Luck Try Next Question :))
       `;
-      quizContainer.appendChild(questionElement);
-    });
+      
+    }
+  
+    currentQuestion++;
+  
+    if (currentQuestion < quizData.length) {
+      loadQuestion();
+    } else {
+      showFinalResult();
+    }
   }
   
-  function submitQuiz() {
-    let score = 0;
-    questions.forEach((question, index) => {
-      const selectedOption = document.querySelector(input[name="q${index}"]:checked);
-      if (selectedOption) {
-        if (selectedOption.value === question.correctAnswer) {
-          score++;
-        }
-      }
-    });
-  
-    alert(Your score: ${score} out of ${questions.length});
+  function showFinalResult() {
+    quizElement.style.display = 'none';
+    optionsElement.style.display = 'none';
+    nextButton.style.display = 'none';
+    resultElement.innerHTML = `Quiz Completed! Your Score: ${score} / ${quizData.length}`;
+    scoreElement.innerHTML = `Score: ${score} / ${quizData.length}`;
   }
   
-  displayQuiz();
+  function restartQuiz() {
+    currentQuestion = 0;
+    score = 0;
+    loadQuestion();
+    quizElement.style.display = 'block';
+    optionsElement.style.display = 'block';
+    nextButton.style.display = 'block';
+    resultElement.innerHTML = '';
+    scoreElement.innerHTML = '';
+  }
+  
+  loadQuestion();
+  
+  nextButton.addEventListener('click', showResult);
+  
